b1e08b021e1461ed48456f0a50395e0fa3015531
mcalsing <marcelocalsing@gmail.com>
2024-02-19 14:47:51 -0300

    Alterado fotos do Sirlon e Dyogo

--- src/app/profiles/page.tsx
+++ src/app/profiles/page.tsx
@@ -14,9 +14,9 @@ export default function Profiles() {
 
         <div className="border-2 border-sky-700 w-full p-5 rounded-xl">
           <h1 className="flex justify-center text-2xl">Dyogo</h1>
-          <div className="flex justify-between p-5">
+          <div className="flex p-5">
             <p className="word-break: break-all text-xl">aasdsdfsasdfasdfsdfdfdsafsdfasdfasdfasdfasdfasdfasdfasdfasdffsdfsdfsdfsdfsdfssdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdfdfsdffsfsdfsdfsdsfsdfsdflkdfjalkdjflk</p>
-            <Image src="/Dyogo.jpeg" alt="dyogo" width={100} height={24} className="hover:animate-bounce"></Image>
+            <Image src="/dyogo2.jpeg" alt="dyogo" width={100} height={24} className="rounded-2xl"></Image>
           </div>
           <div className="flex flex-row-reverse space-x-6 space-x-reverse pr-7">
             <a href="//github.com/DyogoMiguel"><Image src="/github2.png" alt="dyogo" width={30} height={24}></Image></a>
@@ -26,61 +26,61 @@ export default function Profiles() {
 
         <div className="border-2 border-sky-700 w-full p-5 rounded-xl">
           <h1 className="flex justify-center text-2xl">João</h1>
-          <div className="flex justify-between p-5">
+          <div className="flex p-5">
             <p className="word-break: break-all text-xl">aasdsdfsasdfasdfsdfdfdsafsdfasdfasdfasdfasdfasdfasdfasdfasdffsdfsdfsdfsdfsdfssdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdfdfsdffsfsdfsdfsdsfsdfsdflkdfjalkdjflk</p>
             <Image src="/Joao.jfif" alt="joao" width={100} height={24} className="hover:animate-spin"></Image>
           </div>
           <div className="flex flex-row-reverse space-x-6 space-x-reverse pr-7">
             <a href="//github.com/Joao-TeKa"><Image src="/github2.png" alt="dyogo" width={30} height={24}></Image></a>
-            <a href="//linkedin.com/in/guilhermeropke/"><Image src="/linkedin-branco.png" alt="dyogo" width={30} height={24}></Image></a>
+            <a href="//linkedin.com/in/guilhermeropke/"><Image src="/linkedin-branco.png" alt="joao" width={30} height={24}></Image></a>
           </div>
         </div>
 
         <div className="border-2 border-sky-700 w-full p-5 rounded-xl">
           <h1 className="flex justify-center text-2xl">León</h1>
-          <div className="flex justify-between p-5">
+          <div className="flex p-5">
             <p className="word-break: break-all text-xl">aasdsdfsasdfasdfsdfdfdsafsdfasdfasdfasdfasdfasdfasdfasdfasdffsdfsdfsdfsdfsdfssdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdfdfsdffsfsdfsdfsdsfsdfsdflkdfjalkdjflk</p>
             <Image src="/Leon.jpeg" alt="leon" width={100} height={24} className="hover:animate-pulse"></Image>
           </div>
           <div className="flex flex-row-reverse space-x-6 space-x-reverse pr-7">
             <a href="//github.com/lnmaximos"><Image src="/github2.png" alt="dyogo" width={30} height={24}></Image></a>
-            <a href="//linkedin.com/in/leonmaximos"><Image src="/linkedin-branco.png" alt="dyogo" width={30} height={24}></Image></a>
+            <a href="//linkedin.com/in/leonmaximos"><Image src="/linkedin-branco.png" alt="leon" width={30} height={24}></Image></a>
           </div>
         </div>
 
         <div className="border-2 border-sky-700 w-full p-5 rounded-xl">
           <h1 className="flex justify-center text-2xl">Marcelo</h1>
-          <div className="flex justify-between p-5">
+          <div className="flex p-5">
             <p className="word-break: break-all text-xl">aasdsdfsasdfasdfsdfdfdsafsdfasdfasdfasdfasdfasdfasdfasdfasdffsdfsdfsdfsdfsdfssdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdfdfsdffsfsdfsdfsdsfsdfsdflkdfjalkdjflk</p>
-            <Image src="/marcelo2.jpeg" alt="marcelo" width={100} height={24} className="hover:animate-pulse"></Image>
+            <Image src="/marcelo2.jpeg" alt="marcelo" width={100} height={24} className="rounded-2xl"></Image>
           </div>
           <div className="flex flex-row-reverse space-x-6 space-x-reverse pr-7">
             <a href="//github.com/mcalsing"><Image src="/github2.png" alt="dyogo" width={30} height={24}></Image></a>
-            <a href="//linkedin.com/in/marcelocalsing/"><Image src="/linkedin-branco.png" alt="dyogo" width={30} height={24}></Image></a>
+            <a href="//linkedin.com/in/marcelocalsing/"><Image src="/linkedin-branco.png" alt="marcelo" width={30} height={24}></Image></a>
           </div>
         </div>
 
         <div className="border-2 border-sky-700 w-full p-5 rounded-xl">
           <h1 className="flex justify-center text-2xl">Sirlon</h1>
-          <div className="flex justify-between p-5">
+          <div className="flex p-5">
             <p className="word-break: break-all text-xl">aasdsdfsasdfasdfsdfdfdsafsdfasdfasdfasdfasdfasdfasdfasdfasdffsdfsdfsdfsdfsdfssdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdfdfsdffsfsdfsdfsdsfsdfsdflkdfjalkdjflk</p>
-            <Image src="/Sirlon.jpeg" alt="sirlon" width={100} height={24} className="hover:animate-spin"></Image>
+            <Image src="/sirlon2.jpeg" alt="sirlon" width={100} height={24} className="rounded-2xl"></Image>
           </div>
           <div className="flex flex-row-reverse space-x-6 space-x-reverse pr-7">
             <a href="//github.com/sirlonjr"><Image src="/github2.png" alt="dyogo" width={30} height={24}></Image></a>
-            <a href="//linkedin.com/in/sirlonbrehm/"><Image src="/linkedin-branco.png" alt="dyogo" width={30} height={24}></Image></a>
+            <a href="//linkedin.com/in/sirlonbrehm/"><Image src="/linkedin-branco.png" alt="sirlon" width={30} height={24}></Image></a>
           </div>
         </div>
 
         <div className="border-2 border-sky-700 w-full p-5 rounded-xl">
           <h1 className="flex justify-center text-2xl">Yean</h1>
-          <div className="flex justify-between p-5">
+          <div className="flex p-5">
             <p className="word-break: break-all text-xl">aasdsdfsasdfasdfsdfdfdsafsdfasdfasdfasdfasdfasdfasdfasdfasdffsdfsdfsdfsdfsdfssdfsdfsdfsdfsdfsdfsdfsdfsdffsdfsdfdfsdffsfsdfsdfsdsfsdfsdflkdfjalkdjflk</p>
             <Image src="/Yean.jpeg" alt="yean" width={100} height={24} className="hover:animate-bounce"></Image>
           </div>
           <div className="flex flex-row-reverse space-x-6 space-x-reverse pr-7">
             <a href="//github.com/yeanjy"><Image src="/github2.png" alt="dyogo" width={30} height={24}></Image></a>
-            <a href="//linkedin.com/in/yeanjychen/"><Image src="/linkedin-branco.png" alt="dyogo" width={30} height={24}></Image></a>
+            <a href="//linkedin.com/in/yeanjychen/"><Image src="/linkedin-branco.png" alt="yean" width={30} height={24}></Image></a>
           </div>
         </div>
 
