52653fad577007fce71521e77020e345c18e7dee
mcalsing <marcelocalsing@gmail.com>
2024-02-21 16:29:35 -0300

    Refeito cards e landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,47 @@
-"use client"
+"use client";
 import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
 import { integrantes } from "@/utils/mocks";
 
 export default function Home() {
   return (
     <main className="w-full h-full flex flex-col items-center text-slate-300 bg-cyan-950">
-      <div className="p-5 w-4/5">
-        <div className="relative w-full bg-slate-100 rounded-lg">
-          <Image src="/software.png" alt="software" width={1000} height={24} className="w-1/2 h-full rounded-lg"></Image>
-          <div className="absolute top-0 right-0 w-full h-full bg-gradient-to-l from-cyan-800 from-40% rounded-lg"></div>
-          <div className="absolute top-0 right-0 w-1/3 bg">
-            <h1 className="text-2xl pt-1 lg:text-5xl lg:pt-14 xl:text-7xl xl:pt-16 ">PreviTech</h1>
-            <h1 className="word-break: break-all xl:text-2xl">Sasdfasdfasdfadfsdkfjaldkfjf</h1>
-            <h1 className="word-break: break-all xl:text-2xl">Sasdfasdfasdfadfsdkfjaldkfjf</h1>
-            <h1 className="word-break: break-all xl:text-2xl">Sasdfasdfasdfadfsdkfjaldkfjf</h1>
-          </div>
+      <div className="flex p-5 w-full h-[600px] bg-cyan-900 xl:px-44 justify-between text-slate-600 max-md:flex-col">
+        <div className="flex flex-col w-1/2 h-full justify-center items-start text-zinc-100">
+          <span className="font-extrabold text-4xl md:text-6xl">PreviTech</span>
+          <span className="text-xl">Utilizamos tecnologias de Machine Learning para desenvolver</span>
+          <span className="text-xl">um modelo preditivo capaz prever a evasão de clientes.</span>
+        </div>
+        <div className="object-cover max-w-[700px]">
+          <Image src="/software.png" alt="software" width={"700"} height={24} className="h-full rounded-xl"></Image>
         </div>
       </div>
       <div className="flex">
         <h1 className="text-4xl pt-6 font-bold">Integrantes da Equipe</h1>
       </div>
-      <div className="w-full flex justify-center md:px-44 px-10 flex-wrap">
-        <div className="grid justify-items-center p-5 xs:grid-cols-1 lg:grid-cols-3 gap-10 md:grid-cols-2 w-full mt-16">
-        {
-          integrantes.map((card, index) => (
-            <div key={index} className="flex flex-col border-2 border-sky-400 rounded-lg pt-16 px-8 pb-8 relative w-[350px] bg-slate-100 text-zinc-700 mb-14">
+      <div className="w-full flex justify-center items-center px-44 mt-24">
+        <div className="flex justify-center flex-wrap gap-10">
+          {integrantes.map((card, index) => (
+            <div key={index} className="flex flex-col border-2 border-sky-600 rounded-lg pt-16 px-8 pb-8 relative w-[360px] bg-slate-100 text-zinc-700 mb-14 hover:bg-slate-200">
               <div className="rounded-full w-24 h-24 absolute object-cover -top-12 border-4 border-sky-950">
                 <Image src={card.imagem} alt="logolinkedin" layout="fill" className="rounded-full"></Image>
               </div>
-              <span className="text-2xl font-bold">{card.nome}</span>
-              <span>{card.funcao}</span>
+              <div className="flex flex-col mb-8">
+                <span className="text-2xl font-bold">{card.nome}</span>
+                <span className="text-sm font-medium">{card.funcao}</span>
+              </div>
               <span>{card.sobre}</span>
-              <div className="flex gap-4 w-full justify-center">
-                <Image src="/linkedin-branco.png" alt="logolinkedin" width={30} height={24} className="invert"></Image>
-                <Image src="/github2.png" alt="logogithub" width={30} height={24} className="invert"></Image>
+              <div className="flex gap-4 w-full justify-center mt-8">
+                <a href={card.linkedin} target="_blank" className="cursor-pointer">
+                  <Image src="/linkedin-branco.png" alt="logolinkedin" width={30} height={24} className="invert"></Image>
+                </a>
+                <a href={card.github} target="_blank" className="cursor-pointer">
+                  <Image src="/github2.png" alt="logogithub" width={30} height={24} className="invert cursor-pointer"></Image>
+                </a>
               </div>
             </div>
-          ))
-        }
+          ))}
         </div>
-        
-
-        
-
       </div>
     </main>
   );
--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -1,34 +1,56 @@
 export const integrantes = [
-{
-	nome: "Marcelo Calsing",
-	imagem: "/leon2.jpeg",
-	funcao: "lskjalkjfa",
-	sobre: "dfadfad",
-	github: "//github.com/mcalsing",
-	linkedin: "//linkedin.com/in/marcelocalsing",
-},
-{
-	nome: "Marcelo Calsing",
-	imagem: "/marcelo2.jpeg",
-	funcao: "lskjalkjfa",
-	sobre: "dfadfad",
-	github: "//github.com/mcalsing",
-	linkedin: "//linkedin.com/in/marcelocalsing",
-},
-{
-	nome: "Marcelo Calsing",
-	imagem: "/marcelo2.jpeg",
-	funcao: "lskjalkjfa",
-	sobre: "dfadfad",
-	github: "//github.com/mcalsing",
-	linkedin: "//linkedin.com/in/marcelocalsing",
-},
-{
-	nome: "Marcelo Calsing",
-	imagem: "/marcelo2.jpeg",
-	funcao: "lskjalkjfa",
-	sobre: "dfadfad",
-	github: "//github.com/mcalsing",
-	linkedin: "//linkedin.com/in/marcelocalsing",
-}
-]
+  {
+    nome: "León Máximus",
+    imagem: "/leon2.jpeg",
+    funcao: "HTML | CSS | JavaScript | Java",
+    sobre:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    github: "//github.com/lnmaximos",
+    linkedin: "//linkedin.com/in/leonmaximos",
+  },
+  {
+    nome: "Marcelo Calsing",
+    imagem: "/marcelo3.png",
+    funcao: "React | Node.js | JavaScript | Python | Java",
+    sobre:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    github: "//github.com/mcalsing",
+    linkedin: "//linkedin.com/in/marcelocalsing",
+  },
+  {
+    nome: "Dyogo Miguel",
+    imagem: "/dyogo3.png",
+    funcao: "HTML | CSS | JavaScript | Java",
+    sobre:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    github: "//github.com/DyogoMiguel",
+    linkedin: "//linkedin.com/in/dyogo-miguel",
+  },
+  {
+    nome: "João Ropke",
+    imagem: "/joao3.png",
+    funcao: "Java | JavaScript | Python",
+    sobre:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    github: "//github.com/Joao-TeKa",
+    linkedin: "//linkedin.com/in/joãoguilhermeropke",
+  },
+  {
+    nome: "Sirlon Brehm",
+    imagem: "/sirlon3.png",
+    funcao: "HTML | CSS | JavaScript | Java",
+    sobre:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    github: "//github.com/sirlonjr",
+    linkedin: "//linkedin.com/in/sirlonbrehm",
+  },
+  {
+    nome: "Yean Jy Chen",
+    imagem: "/yean3.png",
+    funcao: "HTML | CSS | JavaScript | Java",
+    sobre:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    github: "//github.com/yeanjy",
+    linkedin: "//linkedin.com/in/yeanjychen",
+  },
+];
