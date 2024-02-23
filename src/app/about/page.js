5fdb932f24f169370742223f6b0f62f34e600741
mcalsing <marcelocalsing@gmail.com>
2024-02-23 09:26:52 -0300

    Retirado apóstrofos das palavra em inglês, estava causando erros no deploy

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -27,37 +27,37 @@ export default function About() {
   print(f'Acurácia de validação: {modelo_rf.score(x_val, y_val)}')`;
 
   return (
-    <main className='lg:pl-28 lg:pr-28 pt-12 2xl:pl-64 2xl:pr-64 md:text-xl text-sm text-slate-300 flex flex-col items-center'>
-      <h1 className='text-5xl pb-10'>Construindo o modelo</h1>
-      <div className='flex flex-col bg-sky-950'>
+    <main className="lg:pl-28 lg:pr-28 pt-12 2xl:pl-64 2xl:pr-64 md:text-xl text-sm text-slate-300 flex flex-col items-center">
+      <h1 className="text-5xl pb-10">Construindo o modelo</h1>
+      <div className="flex flex-col bg-sky-950">
         <div class="indent-5 flex flex-col">
-          <p className='break-all'>So I started to walk into the water. I won't lie to you boys, I was
+          <p className="break-all">So I started to walk into the water. I wont lie to you boys, I was
           terrified. But I pressed on, and as I made my way past the breakers
           a strange fffffffff                ffffffcalm came over me. I do           
-          n't know if it was divine intervention
+          nt know if it was divine intervention
           or the kinship of all living things but I tell you Jerry at that moment,
           I <em>was</em> a marine biologist.</p>   
 
 
-          <p>A strange calm came over me. I don't know if it was divine intervention
+          <p>A strange calm came over me. I dont know if it was divine intervention
           or the kinship of all living things but I tell you Jerry at that moment,
           I <em>was</em> a marine biologist.</p>
 
           adfljadflkajsdflkajsdflçaksdjflçaskdjflçadkjf
           <p>asdfasdfasdf</p>
         </div>
-        <div className='bg-cyan-900 text-slate-200 w-full pb-8 rounded-sm pl-5 mb-8 mt-8 relative'>
-          <span className='absolute -top-8 right-0 border-4 border-cyan-900 text-sm rounded-sm p-1 hover:bg-cyan-900'>Block of code</span>
+        <div className="bg-cyan-900 text-slate-200 w-full pb-8 rounded-sm pl-5 mb-8 mt-8 relative">
+          <span className="absolute -top-8 right-0 border-4 border-cyan-900 text-sm rounded-sm p-1 hover:bg-cyan-900">Block of code</span>
           <CodeBlock language="python">{code}</CodeBlock>
         </div>
-        <p>A strange calm came over me. I don't know if it was divine intervention
+        <p>A strange calm came over me. I dont know if it was divine intervention
           or the kinship of all living things but I tell you Jerry at that moment,
           I <em>was</em> a marine biologist.</p>
 
         adfljadflkajsdflkajsdflçaksdjflçaskdjflçadkjf
         <p>asdfasdfasdf</p>
-        <div className='bg-cyan-900 text-slate-200 w-full pb-8 rounded-sm pl-5 mb-8 mt-8 relative'>
-          <span className='absolute -top-8 right-0 border-4 border-cyan-900 text-sm rounded-sm p-1 hover:bg-cyan-900'>Block of code</span>
+        <div className="bg-cyan-900 text-slate-200 w-full pb-8 rounded-sm pl-5 mb-8 mt-8 relative">
+          <span className="absolute -top-8 right-0 border-4 border-cyan-900 text-sm rounded-sm p-1 hover:bg-cyan-900">Block of code</span>
           <CodeBlock language="python">{code}</CodeBlock>
         </div>
       </div>
